8138c877a65be007c7239a6aa4508e2b9ba43890
Sugiura,N <sgurn6i@gmail.com>
2016-10-12 17:16:39 +0900

    test_leg1.lua: avoid gc seg.fault

--- test_leg1.lua
+++ test_leg1.lua
@@ -11,19 +11,21 @@ local leg_amt = 2
 local servos_per_leg = 3
 local chs_per_leg = 4
 local jt = {}
+local pwms = {}
 for ix = 0, leg_amt - 1 do
    jt[chs_per_leg * ix + 0] = c1:create_joint("j_hip"..ix)
    jt[chs_per_leg * ix + 1] = c1:create_joint("j_thigh"..ix)
    jt[chs_per_leg * ix + 2] = c1:create_joint("j_shin"..ix)
    print(jt[chs_per_leg * ix]:get_name())
    for jx = 0, servos_per_leg - 1 do
-      local pwmservo_a = create_pwmservo()
       local ch = chs_per_leg * ix + jx;
+      pwms[ch] = create_pwmservo()
       print(ix, jx, "ch "..ch)
-      pwmservo_a:init(ch, "rs304md" )
-      jt[ch]:attach_pwmservo( pwmservo_a )
+       pwms[ch]:init(ch, "rs304md" )
+      jt[ch]:attach_pwmservo(  pwms[ch] )
    end
 end
+collectgarbage("collect")
 -- reset to zero, wait 1sec
 print("0deg");
 for ix = 0, leg_amt - 1 do
@@ -46,11 +48,8 @@ for ix = 0, leg_amt - 1 do
    jt[chs_per_leg * ix + 2]:target(0)
    b1:do_em_in(500)
 end
-local count = 0
 while 1 do
-   print(count)
-   b1:do_em_in(50)
-   count = count + 1
+   b1:do_em_in(500)
 end
 
 -- end
